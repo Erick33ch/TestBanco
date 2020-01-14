68f0b2add4fd6fd0f53f55229300c7802c4a0736
denny <denny@DESKTOP-A282MDG>
2020-01-14 18:20:08 +0100

    Refactorizar

--- src/es/studium/Banco/TestBanco.java
+++ src/es/studium/Banco/TestBanco.java
@@ -2,39 +2,50 @@ package es.studium.Banco;
 
 public class TestBanco {
 	public static void main(String[] args) {
-		/*NUESTRO CÓDIGO FUENTE*/
+		
 		/* Antonio y Beatriz se hacen cliente del banco */
 		Cliente antonio = new Cliente("123456789Z", "Antonio Alonso", "Av. Pueblo Saharaui, s/n");
 		Cliente beatriz = new Cliente("987654321A", "Beatriz Benítez", "Calle Sol, 4");
  
 		/* Por defecto, todas las cuentas nuevas tienen 100€ */
-		Cuenta cuentaAntonio = new Cuenta(48151, 100, antonio);
-		Cuenta cuentaBeatriz = new Cuenta(62342, 100, beatriz);
+		Cuenta cuAnto = new Cuenta(48151, 100, antonio);
+		Cuenta cuBea = new Cuenta(62342, 100, beatriz);
  
 		/* Antonio y Beatriz consultan el saldo */
-		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "
-				+ cuentaAntonio.getSaldo() + " euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "
-				+ cuentaBeatriz.getSaldo() + " euros.");
+		System.out.println(consultarSaldo(cuAnto));
+		System.out.println(consultarSaldo(cuBea));
  
 		/* Beatriz transfiere 50€ a Antonio */
-		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() - 50);
-		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() + 50);
+		cuBea.setSaldo(transferirSaldo(cuBea));
+		cuAnto.setSaldo(transferirSaldo2(cuAnto));
  
 		/* Antonio y Beatriz vuelven a consultar para comprobar que todo ha ido bien */
-		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "
-				+ cuentaAntonio.getSaldo() + " euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "
-				+ cuentaBeatriz.getSaldo() + " euros.");
+		System.out.println(consultarSaldo(cuAnto));
+		System.out.println(consultarSaldo(cuBea));
  
 		/* Antonio gana 100€ en una rifa y hace un ingreso en su cuenta */
-		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() + 100);
+		double ingresos = cuAnto.getSaldo() + 100;
+		cuAnto.setSaldo(ingresos);
  
 		/* Beatriz tiene que pagar 30€ a hacienda y retira el dinero */
-		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() - 30);
+		double gastoSaldo = cuBea.getSaldo() - 30;
+		cuBea.setSaldo(gastoSaldo);
  
 		/* Antonio transfiere 50€ a Beatriz */
-		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() - 50);
-		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() + 50);
+		cuAnto.setSaldo(transferirSaldo(cuAnto));
+		cuBea.setSaldo(transferirSaldo2(cuBea));
+	}
+
+	private static double transferirSaldo2(Cuenta cuentaBeatriz) {
+		return cuentaBeatriz.getSaldo() + 50;
+	}
+
+	private static double transferirSaldo(Cuenta cuentaAntonio) {
+		return cuentaAntonio.getSaldo() - 50;
+	}
+
+	private static String consultarSaldo(Cuenta cuentaAntonio) {
+		return "La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "
+				+ cuentaAntonio.getSaldo() + " euros.";
 	}
 }
