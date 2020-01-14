bbf87bfea41588bb8f0ed5f321cec147063a9cb1
denny <denny@DESKTOP-A282MDG>
2020-01-14 18:41:58 +0100

    Comeentarios realizados y un poco de JAVAdoc

--- src/es/studium/Banco/TestBanco.java
+++ src/es/studium/Banco/TestBanco.java
@@ -2,41 +2,59 @@ package es.studium.Banco;
 
 public class TestBanco {
 	public static void main(String[] args) {
-		
+		/**
+		 * @author denny
+		 * @version 1.2
+		 * @año 2020
+		 */
 		/* Antonio y Beatriz se hacen cliente del banco */
 		Cliente antonio = new Cliente("123456789Z", "Antonio Alonso", "Av. Pueblo Saharaui, s/n");
 		Cliente beatriz = new Cliente("987654321A", "Beatriz Benítez", "Calle Sol, 4");
- 
+		//Renombramos los nombres para hacerlos más pequeños
 		/* Por defecto, todas las cuentas nuevas tienen 100€ */
 		Cuenta cuAnto = new Cuenta(48151, 100, antonio);
 		Cuenta cuBea = new Cuenta(62342, 100, beatriz);
- 
+		/**
+		 * Extraer metodo 
+		 * @return saldo de sus respectivas cuentas
+		 */
 		/* Antonio y Beatriz consultan el saldo */
 		System.out.println(consultarSaldo(cuAnto));
 		System.out.println(consultarSaldo(cuBea));
- 
+		/**
+		 * Extraer metodo
+		 * @return bea transfiere 50€ a Antonio
+		 */
 		/* Beatriz transfiere 50€ a Antonio */
 		cuBea.setSaldo(transferirSaldo(cuBea));
-		cuAnto.setSaldo(transferirSaldo2(cuAnto));
+		cuAnto.setSaldo(recibirSaldo(cuAnto));
  
 		/* Antonio y Beatriz vuelven a consultar para comprobar que todo ha ido bien */
 		System.out.println(consultarSaldo(cuAnto));
 		System.out.println(consultarSaldo(cuBea));
- 
+		//En estas Extragimos metodo y varibles
 		/* Antonio gana 100€ en una rifa y hace un ingreso en su cuenta */
-		double ingresos = cuAnto.getSaldo() + 100;
+		double ingresos = suma100(cuAnto);
 		cuAnto.setSaldo(ingresos);
  
 		/* Beatriz tiene que pagar 30€ a hacienda y retira el dinero */
-		double gastoSaldo = cuBea.getSaldo() - 30;
+		double gastoSaldo = resta30(cuBea);
 		cuBea.setSaldo(gastoSaldo);
  
 		/* Antonio transfiere 50€ a Beatriz */
 		cuAnto.setSaldo(transferirSaldo(cuAnto));
-		cuBea.setSaldo(transferirSaldo2(cuBea));
+		cuBea.setSaldo(recibirSaldo(cuBea));
 	}
 
-	private static double transferirSaldo2(Cuenta cuentaBeatriz) {
+	private static double resta30(Cuenta cuBea) {
+		return cuBea.getSaldo() - 30;
+	}
+
+	private static double suma100(Cuenta cuAnto) {
+		return cuAnto.getSaldo() + 100;
+	}
+
+	private static double recibirSaldo(Cuenta cuentaBeatriz) {
 		return cuentaBeatriz.getSaldo() + 50;
 	}
 
